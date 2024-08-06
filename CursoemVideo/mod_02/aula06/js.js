e0cbf0de4d3eb6bf8c0a5405e481ec130608c3e0
ddener-nardi <dener@oxigenweb.com.br>
2024-08-06 16:53:38 -0300

    studying js. day3

---
+++ CursoemVideo/mod_02/aula06/js.js
@@ -0,0 +1,5 @@
+var n1 = Number(window.prompt(`Digite um número:`))
+        var n2 = Number(window.prompt(`Digite outro número:`))
+        var sum = n1 + n2
+        window.alert(`A soma dos valores é ${sum.toString()}!`)
+        console.log(typeof sum)
