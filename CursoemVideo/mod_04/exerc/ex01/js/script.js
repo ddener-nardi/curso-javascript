a611bbb3fad8503930ffa20f1f783e7d01bf0c7e
ddener-nardi <dener@oxigenweb.com.br>
2024-08-07 16:44:49 -0300

    end exercise 2

--- CursoemVideo/mod_04/exerc/ex01/js/script.js
+++ CursoemVideo/mod_04/exerc/ex01/js/script.js
@@ -17,3 +17,40 @@ function carregar(){
         document.body.style.background = "#0A6C85"
     }
 }
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano =  document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gender = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'img')
+        if  (fsex[0].checked) {
+            gender = 'Homem'
+            if (idade >= 0 && idade < 12){
+                img.setAttribute('src', '../imgs/homem-crianca.jpg')
+            } else if(idade > 12 && idade < 25){
+                img.setAttribute('src', '../imgs/homem-jovem.jpg')
+            } else {
+                img.setAttribute('src', '../imgs/homem-idoso.jpg')
+            }
+        } else if(fsex[1].checked){
+            gender = 'Mulher'
+            if (idade >= 0 && idade < 12){
+                img.setAttribute('src', '../imgs/mulher-crianca.jpg')
+            } else if(idade > 12 && idade < 25){
+                img.setAttribute('src', '../imgs/mulher-jovem.jpg')
+            } else {
+                img.setAttribute('src', '../imgs/mulher-idoso.jpg')
+            }
+        }
+        res.style.textalign = 'center'
+        res.innerHTML = `Detectamos ${gender} com ${idade} anos.`
+        res.appendChild(img)
+    }     
+}
